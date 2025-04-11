fc3668a2a9e171daab2b4c0767930bf1f3f0f2c1
Vipul Rajan <vipul.s.paul@gmail.com>
2025-04-11 23:22:41 +0530

    code restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import { useHookstate } from "@hookstate/core";
 import { platform } from "@tauri-apps/api/os";
-import { homeDir } from "@tauri-apps/api/path";
-import { invoke } from "@tauri-apps/api/tauri";
 import { useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import BackgroundPlate from "./components/background-plate";
 import TabbedWindow from "./components/tabbed-window";
-import { ConfigNameAndPath } from "./lib/bindings/ConfigNameAndPath";
-import { MinPrinterModelJsonSchema } from "./lib/bindings/MinPrinterModelJsonSchema";
-import { MinPrinterVariantJsonSchema } from "./lib/bindings/MinPrinterVariantJsonSchema";
-import { VendorJsonSchema } from "./lib/bindings/VendorJsonSchema";
-import { checkDirectoryExists } from "./lib/commons";
 import {
-  directoryDefaults,
-  INSTALLED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY,
-  INSTALLED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY_MACOS,
-  LOADED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY,
-  LOADED_USER_PROFILES_BASE_SUBDIRECTORY_DIRECTORY,
-  LOADED_USER_PROFILES_MACHINE_SUBDIRECTORY_DIRECTORY,
-  LOADED_USER_PROFILES_SUBDIRECTORY_DIRECTORY,
-} from "./lib/constants";
-import { fileProperty, globalState } from "./lib/state-store";
+  dataPrinterConfigLoader,
+  modelConfigLoader,
+  setOsAndDefaultDirectories as setDefaultDirectories,
+  systemPrinterConfigLoader,
+  vendorConfigLoader,
+} from "./lib/commons";
+import { globalState } from "./lib/state-store";
 
 function App() {
   const {
@@ -41,341 +32,47 @@ function App() {
   useEffect(() => {
     platform().then((a) => {
       os.set(a);
-
-      const setDefaultDirectories = async () => {
-        switch (a) {
-          case "darwin": {
-            checkDirectoryExists(
-              directoryDefaults.darwin.installationDirectory
-            ).then((res) => {
-              if (res) {
-                orcaInstallationPath.set(
-                  directoryDefaults.darwin.installationDirectory
-                );
-                toast("Set default installation directory", {
-                  type: "success",
-                });
-              } else
-                toast(
-                  "Default installation directory does not exist.\nPlease set manually",
-                  { type: "error" }
-                );
-            });
-
-            homeDir().then((homeDirectory) => {
-              const dataDirectory =
-                homeDirectory + directoryDefaults.darwin.dataSubdirectory;
-              checkDirectoryExists(dataDirectory).then((res) => {
-                if (res) {
-                  orcaDataDirectory.set(dataDirectory);
-                  toast("Set default data directory", {
-                    type: "success",
-                  });
-                } else
-                  toast(
-                    "Default data directory does not exist.\nPlease set manually",
-                    { type: "error" }
-                  );
-              });
-            });
-
-            break;
-          }
-
-          case "win32": {
-            checkDirectoryExists(
-              directoryDefaults.win32.installationDirectory
-            ).then((res) => {
-              if (res) {
-                orcaInstallationPath.set(
-                  directoryDefaults.win32.installationDirectory
-                );
-                toast("Set default installation directory", {
-                  type: "success",
-                });
-              } else
-                toast(
-                  "Default installation directory does not exist.\nPlease set manually",
-                  { type: "error" }
-                );
-            });
-
-            homeDir().then((homeDirectory) => {
-              const dataDirectory =
-                homeDirectory + directoryDefaults.win32.dataSubdirectory;
-              checkDirectoryExists(dataDirectory).then((res) => {
-                if (res) {
-                  orcaDataDirectory.set(dataDirectory);
-                  toast("Set default data directory", {
-                    type: "success",
-                  });
-                } else
-                  toast(
-                    "Default data directory does not exist.\nPlease set manually",
-                    { type: "error" }
-                  );
-              });
-            });
-
-            break;
-          }
-        }
-      };
-
-      setDefaultDirectories();
+      setDefaultDirectories(a, orcaInstallationPath, orcaDataDirectory);
     });
   }, []);
 
-  const get_installed_system_profiles_subdirectory_directory = () => {
-    console.log(os.get({ stealth: true }));
-    if (os.get({ stealth: true }) == "darwin")
-      return INSTALLED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY_MACOS;
-    else return INSTALLED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY;
-  };
-
   useEffect(() => {
-    const config_loader = async () => {
-      try {
-        if (orcaInstallationPath.get({ stealth: true })) {
-          console.log(
-            orcaInstallationPath.get({ stealth: true }) +
-              get_installed_system_profiles_subdirectory_directory()
-          );
-          const vendorConfigsRead: Record<string, VendorJsonSchema> =
-            await invoke("load_all_system_vendor_profiles", {
-              path:
-                orcaInstallationPath.get({ stealth: true }) +
-                get_installed_system_profiles_subdirectory_directory(),
-            });
-
-          vendorConfigs.set(vendorConfigsRead);
-          errLoadingInstallationPath.set(undefined);
-          toast("Loaded system vendor configurations", { type: "success" });
-        } else {
-          vendorConfigs.set({});
-          errLoadingInstallationPath.set(undefined);
-        }
-      } catch (error: any) {
-        vendorConfigs.set({});
-        toast(error, { type: "error" });
-        errLoadingInstallationPath.set(error);
-      }
-    };
-
-    config_loader();
+    vendorConfigLoader(
+      os,
+      orcaInstallationPath,
+      vendorConfigs,
+      errLoadingInstallationPath
+    );
   }, [orcaInstallationPath]);
 
   useEffect(() => {
-    const config_loader = async () => {
-      try {
-        if (
-          orcaDataDirectory.get({ stealth: true }) &&
-          !errLoadingDataPath.get({ stealth: true })
-        ) {
-          const vendorConfigsRead: Record<string, VendorJsonSchema> =
-            await invoke("load_all_system_vendor_profiles", {
-              path:
-                orcaDataDirectory.get({ stealth: true }) +
-                LOADED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY,
-            });
-
-          Object.keys(vendorConfigsRead).map(async (key) => {
-            const vendorConfig = vendorConfigsRead[key];
-            const machine_list =
-              vendorConfig.machine_list as ConfigNameAndPath[];
-
-            const printerConfigsParsed: (MinPrinterModelJsonSchema & {
-              fileName: string;
-            })[] = await invoke("load_all_printer_presets", {
-              path:
-                orcaDataDirectory.get({ stealth: true }) +
-                LOADED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY +
-                "/" +
-                key,
-              configNameAndPaths: machine_list,
-            });
-
-            for (let i = 0; i < machine_list.length; i++) {
-              printerConfigsParsed[i].fileName =
-                orcaDataDirectory.get({ stealth: true }) +
-                LOADED_SYSTEM_PROFILES_SUBDIRECTORY_DIRECTORY +
-                "/" +
-                key +
-                "/" +
-                machine_list[i].sub_path;
-
-              loadedSystemPrinterConfigs[key].merge({
-                [machine_list[i].name]: printerConfigsParsed[i],
-              });
-            }
-          });
-
-          const userMachineDirectory =
-            orcaDataDirectory.get({ stealth: true }) +
-            LOADED_USER_PROFILES_SUBDIRECTORY_DIRECTORY +
-            LOADED_USER_PROFILES_MACHINE_SUBDIRECTORY_DIRECTORY;
-          const userMachineBaseDirectory =
-            userMachineDirectory +
-            LOADED_USER_PROFILES_BASE_SUBDIRECTORY_DIRECTORY;
-
-          const inheritedPrinterConfigs: [
-            String,
-            MinPrinterVariantJsonSchema
-          ][] = await invoke("load_all_user_printer_profiles_in_dir", {
-            path: userMachineDirectory,
-          });
-
-          try {
-            const basePrinterConfigs: [String, MinPrinterVariantJsonSchema][] =
-              await invoke("load_all_user_printer_profiles_in_dir", {
-                path: userMachineBaseDirectory,
-              });
-
-            basePrinterConfigs.forEach(([path, config]) => {
-              const configWithFileName: MinPrinterVariantJsonSchema &
-                fileProperty = { ...config, fileName: path.toString() };
-              loadedUserPrinterConfigs.merge({
-                [config.name]: configWithFileName,
-              });
-            });
-          } catch (error: any) {
-            console.log("base directory does not exist, skipping");
-          }
-
-          inheritedPrinterConfigs.forEach(([path, config]) => {
-            const configWithFileName: MinPrinterVariantJsonSchema &
-              fileProperty = { ...config, fileName: path.toString() };
-            loadedUserPrinterConfigs.merge({
-              [config.name]: configWithFileName,
-            });
-          });
-
-          errLoadingDataPath.set(undefined);
-          toast("Loaded user printer profiles", { type: "success" });
-        } else {
-          loadedSystemPrinterConfigs.set({});
-          errLoadingDataPath.set(undefined);
-        }
-      } catch (error: any) {
-        loadedSystemPrinterConfigs.set({});
-        errLoadingDataPath.set(error);
-        toast(error, { type: "error" });
-      }
-    };
-
-    config_loader();
+    dataPrinterConfigLoader(
+      orcaDataDirectory,
+      loadedSystemPrinterConfigs,
+      loadedUserPrinterConfigs,
+      errLoadingDataPath
+    );
   }, [orcaDataDirectory]);
 
   useEffect(() => {
-    const config_loader = async () => {
-      try {
-        if (
-          orcaInstallationPath.get({ stealth: true }) &&
-          !errLoadingInstallationPath.get({ stealth: true })
-        ) {
-          vendorConfigs.keys.map(async (key) => {
-            const vendorConfig = vendorConfigs[key].get({ stealth: true });
-            const machine_model_list =
-              vendorConfig.machine_model_list as ConfigNameAndPath[];
-            const modelConfigsParsed: (MinPrinterModelJsonSchema & {
-              fileName: string;
-            })[] = await invoke("load_all_printer_model_presets", {
-              path:
-                orcaInstallationPath.get({ stealth: true }) +
-                get_installed_system_profiles_subdirectory_directory() +
-                "/" +
-                key,
-              configNameAndPaths: machine_model_list,
-            });
-
-            for (let i = 0; i < machine_model_list.length; i++) {
-              modelConfigsParsed[i].fileName =
-                orcaInstallationPath.get({ stealth: true }) +
-                get_installed_system_profiles_subdirectory_directory() +
-                "/" +
-                key +
-                "/" +
-                machine_model_list[i].sub_path;
-              modelConfigs[machine_model_list[i].name].set(
-                modelConfigsParsed[i]
-              );
-            }
-          });
-
-          toast("Loaded system model configs", { type: "success" });
-        } else {
-          modelConfigs.set({});
-        }
-      } catch (error: any) {
-        toast(error, { type: "error" });
-        console.log(error);
-        modelConfigs.set({});
-      }
-    };
-
-    config_loader();
+    modelConfigLoader(
+      os,
+      orcaInstallationPath,
+      vendorConfigs,
+      modelConfigs,
+      errLoadingInstallationPath
+    );
   }, [vendorConfigs]);
 
   useEffect(() => {
-    const config_loader = async () => {
-      try {
-        if (
-          orcaInstallationPath.get({ stealth: true }) &&
-          !errLoadingInstallationPath.get({ stealth: true })
-        ) {
-          vendorConfigs.keys.map(async (key) => {
-            const vendorConfig = vendorConfigs[key].get({ stealth: true });
-            const machine_list =
-              vendorConfig.machine_list as ConfigNameAndPath[];
-            const printerConfigsParsed: ({
-              Ok?: MinPrinterVariantJsonSchema;
-              Err?: string;
-            } & {
-              fileName: string;
-            })[] = await invoke("load_all_printer_presets", {
-              path:
-                orcaInstallationPath.get({ stealth: true }) +
-                get_installed_system_profiles_subdirectory_directory() +
-                "/" +
-                key,
-              configNameAndPaths: machine_list,
-            });
-
-            for (let i = 0; i < machine_list.length; i++) {
-              printerConfigsParsed[i].fileName =
-                orcaInstallationPath.get({ stealth: true }) +
-                get_installed_system_profiles_subdirectory_directory() +
-                "/" +
-                key +
-                "/" +
-                machine_list[i].sub_path;
-
-              installedPrinterConfigs[key].merge({
-                [machine_list[i].name]: printerConfigsParsed[i],
-              });
-
-              if (printerConfigsParsed[i].Ok?.instantiation)
-                instantiatedInstalledPrinterConfigs.merge({
-                  [machine_list[i].name]: {
-                    ...printerConfigsParsed[i].Ok!,
-                    fileName: printerConfigsParsed[i].fileName,
-                  },
-                });
-            }
-          });
-
-          toast("Loaded system machine configs", { type: "success" });
-        } else {
-          installedPrinterConfigs.set({});
-        }
-      } catch (error: any) {
-        toast(error, { type: "error" });
-        console.log(error);
-        installedPrinterConfigs.set({});
-      }
-    };
-
-    config_loader();
+    systemPrinterConfigLoader(
+      os,
+      orcaInstallationPath,
+      vendorConfigs,
+      installedPrinterConfigs,
+      instantiatedInstalledPrinterConfigs,
+      errLoadingInstallationPath
+    );
   }, [vendorConfigs]);
 
   return (
