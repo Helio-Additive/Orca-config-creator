a5f005eadcb520727a197c576ea1ede7902e9faa
Vipul Rajan <vipul.s.paul@gmail.com>
2025-05-25 15:57:41 +0530

    performance optimisation

--- src/components/tab-panels/printer-config-tab.tsx
+++ src/components/tab-panels/printer-config-tab.tsx
@@ -6,10 +6,11 @@ import {
   exportFlattened,
   newFile,
 } from "../../lib/commons";
-import { globalState } from "../../lib/state-store";
+import { appState, appStateObject, globalState } from "../../lib/state-store";
 import ConfigItem from "./config-list/config-item";
 import TopButton from "./config-list/config-item-components/top-button";
 import ConfigTabTemplate from "./config-tab-template";
+import { useEffect, useRef } from "react";
 
 export default function PrinterConfigTab() {
   const {
@@ -17,6 +18,24 @@ export default function PrinterConfigTab() {
     loadedSystemPrinterConfigs,
     loadedUserPrinterConfigs,
   } = useHookstate(globalState);
+
+  const {
+    itemVisibilityState: { printer: itemVisibility },
+  } = useHookstate(appState);
+
+  const currentIndexes = useRef({ installed: 0, loadedSystem: 0, user: 0 });
+
+  useEffect(() => {
+    return () => {
+      itemVisibility.installed.set(
+        appStateObject.itemVisibilityState.printer.installed
+      );
+      itemVisibility.loadedSystem.set(
+        appStateObject.itemVisibilityState.printer.loadedSystem
+      );
+      itemVisibility.user.set(appStateObject.itemVisibilityState.printer.user);
+    };
+  }, []);
 
   const navigate = useNavigate();
 
@@ -26,100 +45,133 @@ export default function PrinterConfigTab() {
     family?: string
   ) => exportFlattened(configName, "printer", location, family);
 
-  const installedConfigs = installedPrinterConfigs.keys.map((key) => {
-    const vendorConfig = installedPrinterConfigs[key];
+  const installedConfigs = installedPrinterConfigs.keys.map(
+    (key, familyIndex) => {
+      if (familyIndex === 0) currentIndexes.current.installed = 0;
 
-    return (
-      <div key={key}>
-        <div className="flex min-h-0 h-fit mt-3 mb-1 pl-3 items-center">
-          <span className="font-semibold text-text-primary text-xl">{key}</span>
+      const vendorConfig = installedPrinterConfigs[key];
 
-          <TopButton
-            onClick={() => newFile("printer", "installed", navigate, key)}
-            Icon={VscNewFile}
-            tooltip="Add new Config"
-            className="text-text-primary ml-2"
-          />
+      return (
+        <div key={key}>
+          <div className="flex min-h-0 h-fit mt-3 mb-1 pl-3 items-center">
+            <span className="font-semibold text-text-primary text-xl">
+              {key}
+            </span>
+
+            <TopButton
+              onClick={() => newFile("printer", "installed", navigate, key)}
+              Icon={VscNewFile}
+              tooltip="Add new Config"
+              className="text-text-primary ml-2"
+            />
+          </div>
+          {vendorConfig.keys.map((printerName) => {
+            const config = vendorConfig.nested(printerName).get();
+
+            if (
+              currentIndexes.current.installed > itemVisibility.installed.get()
+            )
+              return <></>;
+
+            currentIndexes.current.installed += 1;
+
+            if (config.Ok) {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={config.Ok.name}
+                  text1={key}
+                  text2={[config.Ok.inherits ?? "base"]}
+                  fileName={config.fileName}
+                  type="printer"
+                  family={key}
+                  allowEdit
+                  configLocation="installed"
+                  flatExportFunction={export_flattened}
+                  allowDelete
+                  index={currentIndexes.current.installed}
+                  itemVisibilityNumberState={itemVisibility.installed}
+                />
+              );
+            } else {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={printerName}
+                  text2={[config.Err!]}
+                  className="bg-transparent-error"
+                  fileName={config.fileName}
+                  type="printer"
+                  configLocation="installed"
+                  index={currentIndexes.current.installed}
+                  itemVisibilityNumberState={itemVisibility.installed}
+                />
+              );
+            }
+          })}
         </div>
-        {vendorConfig.keys.map((printerName) => {
-          const config = vendorConfig.nested(printerName).get();
+      );
+    }
+  );
 
-          if (config.Ok) {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={config.Ok.name}
-                text1={key}
-                text2={[config.Ok.inherits ?? "base"]}
-                fileName={config.fileName}
-                type="printer"
-                family={key}
-                allowEdit
-                configLocation="installed"
-                flatExportFunction={export_flattened}
-                allowDelete
-              />
-            );
-          } else {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={printerName}
-                text2={[config.Err!]}
-                className="bg-transparent-error"
-                fileName={config.fileName}
-                type="printer"
-                configLocation="installed"
-              />
-            );
-          }
-        })}
-      </div>
-    );
-  });
+  const loadedSystemConfigs = loadedSystemPrinterConfigs.keys.map(
+    (key, familyIndex) => {
+      if (familyIndex === 0) currentIndexes.current.loadedSystem = 0;
 
-  const loadedSystemConfigs = loadedSystemPrinterConfigs.keys.map((key) => {
-    const vendorConfig = loadedSystemPrinterConfigs[key];
+      const vendorConfig = loadedSystemPrinterConfigs[key];
 
-    return (
-      <div key={key}>
-        <span className="font-semibold text-text-primary text-xl mb-1 pl-3 mt-3">
-          {key}
-        </span>
-        {vendorConfig.keys.map((printerName) => {
-          const config = vendorConfig.nested(printerName).get();
+      return (
+        <div key={key}>
+          <span className="font-semibold text-text-primary text-xl mb-1 pl-3 mt-3">
+            {key}
+          </span>
+          {vendorConfig.keys.map((printerName) => {
+            const config = vendorConfig.nested(printerName).get();
 
-          if (config.Ok) {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={config.Ok.name}
-                text2={[config.Ok.inherits ?? "base"]}
-                fileName={config.fileName}
-                type="printer"
-                family={key}
-                configLocation="loaded_system"
-              />
-            );
-          } else {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={printerName}
-                text2={[config.Err!]}
-                className="bg-transparent-error"
-                fileName={config.fileName}
-                type="printer"
-                configLocation="loaded_system"
-              />
-            );
-          }
-        })}
-      </div>
-    );
-  });
+            if (
+              currentIndexes.current.loadedSystem >
+              itemVisibility.loadedSystem.get()
+            )
+              return <></>;
 
-  const loadedUserConfigs = loadedUserPrinterConfigs.keys.map((key) => {
+            currentIndexes.current.loadedSystem += 1;
+
+            if (config.Ok) {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={config.Ok.name}
+                  text2={[config.Ok.inherits ?? "base"]}
+                  fileName={config.fileName}
+                  type="printer"
+                  family={key}
+                  configLocation="loaded_system"
+                  index={currentIndexes.current.loadedSystem}
+                  itemVisibilityNumberState={itemVisibility.loadedSystem}
+                />
+              );
+            } else {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={printerName}
+                  text2={[config.Err!]}
+                  className="bg-transparent-error"
+                  fileName={config.fileName}
+                  type="printer"
+                  configLocation="loaded_system"
+                  index={currentIndexes.current.loadedSystem}
+                  itemVisibilityNumberState={itemVisibility.loadedSystem}
+                />
+              );
+            }
+          })}
+        </div>
+      );
+    }
+  );
+
+  const loadedUserConfigs = loadedUserPrinterConfigs.keys.map((key, index) => {
     const machineConfig = loadedUserPrinterConfigs.nested(key).get();
 
     return (
@@ -137,6 +189,8 @@ export default function PrinterConfigTab() {
         type="printer"
         configLocation="user"
         allowDelete
+        index={index}
+        itemVisibilityNumberState={itemVisibility.user}
       />
     );
   });
--- src/components/tab-panels/process-config-tab.tsx
+++ src/components/tab-panels/process-config-tab.tsx
@@ -1,8 +1,9 @@
 import { useHookstate } from "@hookstate/core";
 import { ConfigLocationType, exportFlattened } from "../../lib/commons";
-import { globalState } from "../../lib/state-store";
+import { appState, appStateObject, globalState } from "../../lib/state-store";
 import ConfigItem from "./config-list/config-item";
 import ConfigTabTemplate from "./config-tab-template";
+import { useEffect, useRef } from "react";
 
 export default function ProcessConfigTab() {
   const {
@@ -11,97 +12,146 @@ export default function ProcessConfigTab() {
     loadedUserProcessConfigs,
   } = useHookstate(globalState);
 
+  const {
+    itemVisibilityState: { process: itemVisibility },
+  } = useHookstate(appState);
+
+  const currentIndexes = useRef({ installed: 0, loadedSystem: 0, user: 0 });
+
+  useEffect(() => {
+    return () => {
+      itemVisibility.installed.set(
+        appStateObject.itemVisibilityState.printer.installed
+      );
+      itemVisibility.loadedSystem.set(
+        appStateObject.itemVisibilityState.printer.loadedSystem
+      );
+      itemVisibility.user.set(appStateObject.itemVisibilityState.printer.user);
+    };
+  }, []);
+
   const export_flattened = async (
     configName: string,
     location: ConfigLocationType,
     family?: string
   ) => exportFlattened(configName, "process", location, family);
 
-  const installedConfigs = installedProcessConfigs.keys.map((key) => {
-    const vendorConfig = installedProcessConfigs[key];
+  const installedConfigs = installedProcessConfigs.keys.map(
+    (key, familyIndex) => {
+      if (familyIndex === 0) currentIndexes.current.installed = 0;
 
-    return (
-      <div key={key}>
-        <span className="font-semibold text-text-primary text-xl mb-1 pl-3 mt-3">
-          {key}
-        </span>
-        {vendorConfig.keys.map((printerName) => {
-          const config = vendorConfig.nested(printerName).get();
+      const vendorConfig = installedProcessConfigs[key];
 
-          if (config.Ok) {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={config.Ok.name}
-                text1={key}
-                text2={[config.Ok.inherits ?? "base"]}
-                fileName={config.fileName}
-                type="process"
-                allowEdit
-                configLocation="installed"
-                flatExportFunction={export_flattened}
-              />
-            );
-          } else {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={printerName}
-                text2={[config.Err!]}
-                className="bg-transparent-error"
-                fileName={config.fileName}
-                type="process"
-                configLocation="installed"
-              />
-            );
-          }
-        })}
-      </div>
-    );
-  });
+      return (
+        <div key={key}>
+          <span className="font-semibold text-text-primary text-xl mb-1 pl-3 mt-3">
+            {key}
+          </span>
+          {vendorConfig.keys.map((printerName) => {
+            const config = vendorConfig.nested(printerName).get();
 
-  const loadedSystemConfigs = loadedSystemProcessConfigs.keys.map((key) => {
-    const vendorConfig = loadedSystemProcessConfigs[key];
+            if (
+              currentIndexes.current.installed > itemVisibility.installed.get()
+            )
+              return <></>;
 
-    return (
-      <div key={key}>
-        <span className="font-semibold text-text-primary text-xl mb-1 pl-3 mt-3">
-          {key}
-        </span>
-        {vendorConfig.keys.map((printerName) => {
-          const config = vendorConfig.nested(printerName).get();
+            currentIndexes.current.installed += 1;
 
-          if (config.Ok) {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={config.Ok.name}
-                text2={[config.Ok.inherits ?? "base"]}
-                fileName={config.fileName}
-                type="process"
-                configLocation="loaded_system"
-                flatExportFunction={export_flattened}
-              />
-            );
-          } else {
-            return (
-              <ConfigItem
-                key={key + printerName}
-                name={printerName}
-                text2={[config.Err!]}
-                className="bg-transparent-error"
-                fileName={config.fileName}
-                type="process"
-                configLocation="loaded_system"
-              />
-            );
-          }
-        })}
-      </div>
-    );
-  });
+            if (config.Ok) {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={config.Ok.name}
+                  text1={key}
+                  text2={[config.Ok.inherits ?? "base"]}
+                  fileName={config.fileName}
+                  type="process"
+                  allowEdit
+                  configLocation="installed"
+                  flatExportFunction={export_flattened}
+                  index={currentIndexes.current.installed}
+                  itemVisibilityNumberState={itemVisibility.installed}
+                />
+              );
+            } else {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={printerName}
+                  text2={[config.Err!]}
+                  className="bg-transparent-error"
+                  fileName={config.fileName}
+                  type="process"
+                  configLocation="installed"
+                  index={currentIndexes.current.installed}
+                  itemVisibilityNumberState={itemVisibility.installed}
+                />
+              );
+            }
+          })}
+        </div>
+      );
+    }
+  );
 
-  const loadedUserConfigs = loadedUserProcessConfigs.keys.map((key) => {
+  const loadedSystemConfigs = loadedSystemProcessConfigs.keys.map(
+    (key, familyIndex) => {
+      if (familyIndex === 0) currentIndexes.current.loadedSystem = 0;
+
+      const vendorConfig = loadedSystemProcessConfigs[key];
+
+      return (
+        <div key={key}>
+          <span className="font-semibold text-text-primary text-xl mb-1 pl-3 mt-3">
+            {key}
+          </span>
+          {vendorConfig.keys.map((printerName) => {
+            const config = vendorConfig.nested(printerName).get();
+
+            if (
+              currentIndexes.current.loadedSystem >
+              itemVisibility.loadedSystem.get()
+            )
+              return <></>;
+
+            currentIndexes.current.loadedSystem += 1;
+
+            if (config.Ok) {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={config.Ok.name}
+                  text2={[config.Ok.inherits ?? "base"]}
+                  fileName={config.fileName}
+                  type="process"
+                  configLocation="loaded_system"
+                  flatExportFunction={export_flattened}
+                  index={currentIndexes.current.loadedSystem}
+                  itemVisibilityNumberState={itemVisibility.loadedSystem}
+                />
+              );
+            } else {
+              return (
+                <ConfigItem
+                  key={key + printerName}
+                  name={printerName}
+                  text2={[config.Err!]}
+                  className="bg-transparent-error"
+                  fileName={config.fileName}
+                  type="process"
+                  configLocation="loaded_system"
+                  index={currentIndexes.current.loadedSystem}
+                  itemVisibilityNumberState={itemVisibility.loadedSystem}
+                />
+              );
+            }
+          })}
+        </div>
+      );
+    }
+  );
+
+  const loadedUserConfigs = loadedUserProcessConfigs.keys.map((key, index) => {
     const machineConfig = loadedUserProcessConfigs.nested(key).get();
 
     return (
@@ -119,6 +169,8 @@ export default function ProcessConfigTab() {
         flatExportFunction={export_flattened}
         configLocation="user"
         allowDelete
+        index={index}
+        itemVisibilityNumberState={itemVisibility.user}
       />
     );
   });
