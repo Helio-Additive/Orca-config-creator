b6225ff5b1097a2c32290a84f23cffcf5a8bf3fd
Vipul Rajan <vipul.s.paul@gmail.com>
2025-06-04 20:23:53 +0530

    search and optimisation working on analysis tab

--- src/components/tab-panels/analysis-tab.tsx
+++ src/components/tab-panels/analysis-tab.tsx
@@ -1,10 +1,33 @@
 import { useHookstate } from "@hookstate/core";
-import { ConfigLocationType, ConfigType } from "../../lib/commons";
-import { globalState } from "../../lib/state-store";
+import {
+  ConfigLocationType,
+  ConfigType,
+  matchesQuery,
+} from "../../lib/commons";
+import { appState, appStateObject, globalState } from "../../lib/state-store";
 import ConfigItem from "./config-list/config-item";
+import { useEffect, useRef } from "react";
 
 export default function AnalysisTab() {
   const { analysisErrors, analysisWarnings } = useHookstate(globalState);
+
+  const {
+    itemVisibilityState: { analyzed: itemVisibility },
+    searchQuery,
+  } = useHookstate(appState);
+
+  const currentIndexes = useRef({ errors: 0, warnings: 0 });
+
+  useEffect(() => {
+    return () => {
+      itemVisibility.errors.set(
+        appStateObject.itemVisibilityState.analyzed.errors
+      );
+      itemVisibility.warnings.set(
+        appStateObject.itemVisibilityState.analyzed.warnings
+      );
+    };
+  }, []);
 
   return (
     <div className="h-full flex flex-col">
@@ -13,16 +36,34 @@ export default function AnalysisTab() {
       </div>
       <div className="flex-1/2 min-h-0 overflow-y-auto">
         {Object.entries(analysisErrors.get()).flatMap(
-          ([fileName, messages]) => {
+          ([fileName, messages], outerIndex) => {
+            if (outerIndex === 0) currentIndexes.current.errors = 0;
             return Object.entries(messages).flatMap(
               ([fieldName, messageDetailsList]) => {
                 return messageDetailsList.map((messageDetails, index) => {
+                  if (
+                    currentIndexes.current.errors >
+                      itemVisibility.errors.get() ||
+                    !matchesQuery(searchQuery.get(), [
+                      fileName,
+                      fieldName,
+                      messageDetails.config_details.family ?? "",
+                      messageDetails.config_details.location,
+                      messageDetails.message.text,
+                      messageDetails.config_details.config_type,
+                    ])
+                  )
+                    return <div key={fileName + fieldName + index}></div>;
+
+                  currentIndexes.current.errors += 1;
+
                   return (
                     <ConfigItem
                       key={fileName + fieldName + index}
                       name={messageDetails.config_details.name}
                       text1={messageDetails.config_details.config_type}
                       text2={[
+                        messageDetails.config_details.family ?? "",
                         messageDetails.config_details.location,
                         messageDetails.message.text,
                       ]}
@@ -36,6 +77,8 @@ export default function AnalysisTab() {
                           .location as ConfigLocationType
                       }
                       className="max-w-[fit] w-fit"
+                      index={currentIndexes.current.errors}
+                      itemVisibilityNumberState={itemVisibility.errors}
                       allowEdit
                     />
                   );
@@ -51,10 +94,27 @@ export default function AnalysisTab() {
       </div>
       <div className="flex-1/2 shrink-1 basis-1/2 min-h-0 overflow-y-auto">
         {Object.entries(analysisWarnings.get()).flatMap(
-          ([fileName, messages]) => {
+          ([fileName, messages], outerIndex) => {
+            if (outerIndex === 0) currentIndexes.current.errors = 0;
             return Object.entries(messages).flatMap(
               ([fieldName, messageDetailsList]) => {
                 return messageDetailsList.map((messageDetails, index) => {
+                  if (
+                    currentIndexes.current.errors >
+                      itemVisibility.errors.get() ||
+                    !matchesQuery(searchQuery.get(), [
+                      fileName,
+                      fieldName,
+                      messageDetails.config_details.family ?? "",
+                      messageDetails.config_details.location,
+                      messageDetails.message.text,
+                      messageDetails.config_details.config_type,
+                    ])
+                  )
+                    return <div key={fileName + fieldName + index}></div>;
+
+                  currentIndexes.current.errors += 1;
+
                   return (
                     <ConfigItem
                       key={fileName + fieldName + index}
@@ -74,6 +134,8 @@ export default function AnalysisTab() {
                           .location as ConfigLocationType
                       }
                       className="max-w-[fit] w-fit"
+                      index={currentIndexes.current.errors}
+                      itemVisibilityNumberState={itemVisibility.errors}
                       allowEdit
                     />
                   );
